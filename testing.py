dc681669b0319640b75d0e4036518dfc6c888225
EShoukry <35610424+EShoukry@users.noreply.github.com>
2019-03-25 14:45:19 -0400

    Python 3 Version of the Code

---
+++ testing.py
@@ -0,0 +1,13 @@
+import pacman
+from game import GameStateData
+from game import Game
+from game import Directions
+from game import Actions
+from util import nearestPoint
+from util import manhattanDistance
+import util, layout
+import sys, types, time, random, os
+
+
+x = getPacmanState()
+print(x)
